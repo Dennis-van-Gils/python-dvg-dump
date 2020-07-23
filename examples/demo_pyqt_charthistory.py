56b76fa825a922f2ac43382a9090ac5d38553ecc
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2020-07-23 23:26:48 +0200

    Working example

--- examples/demo_pyqt_charthistory.py
+++ examples/demo_pyqt_charthistory.py
@@ -2,19 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import time
 
 import numpy as np
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets as QtWid
 import pyqtgraph as pg
 
-from dvg_debug_functions import dprint
 from dvg_pyqt_charthistory import ChartHistory
 from dvg_pyqt_controls import SS_GROUP
 from dvg_qdeviceio import QDeviceIO
 
 if 1:
+    print("OpenGL acceleration: Enabled")
     pg.setConfigOptions(useOpenGL=True)
     pg.setConfigOptions(antialias=True)
     pg.setConfigOptions(enableExperimental=True)
@@ -23,11 +22,9 @@ pg.setConfigOption("leftButtonPan", False)
 
 # Constants
 Fs = 10000  # Sampling rate of the simulated data [Hz]
-WORKER_DAQ_INTERVAL_MS = round(1000 / 50)  # [ms]
-CHART_DRAW_INTERVAL_MS = round(1000 / 25)  # [ms]
+WORKER_DAQ_INTERVAL_MS = round(1000 / 100)  # [ms]
+CHART_DRAW_INTERVAL_MS = round(1000 / 50)  # [ms]
 CHART_HISTORY_TIME = 10  # 10 [s]
-
-prev_tick = time.perf_counter()
 
 # ------------------------------------------------------------------------------
 #   MainWindow
@@ -47,6 +44,7 @@ class MainWindow(QtWid.QWidget):
 
         p = {"color": "#CCC", "font-size": "10pt"}
         self.pi_chart.showGrid(x=1, y=1)
+        self.pi_chart.setTitle("title")
         self.pi_chart.setLabel("bottom", text="history (sec)", **p)
         self.pi_chart.setLabel("left", text="amplitude", **p)
         self.pi_chart.setRange(
@@ -62,30 +60,46 @@ class MainWindow(QtWid.QWidget):
             linked_curve=self.pi_chart.plot(pen=PEN_01),
         )
 
-        # Options
-        self.qpbt_option_1 = QtWid.QPushButton("Pause/unpause")
+        # `Obtained rates`
+        self.qlbl_DAQ_rate = QtWid.QLabel("")
+        self.qlbl_DAQ_rate.setAlignment(QtCore.Qt.AlignRight)
+        self.qlbl_DAQ_rate.setMinimumWidth(50)
+        self.qlbl_chart_rate = QtWid.QLabel("")
+        self.qlbl_chart_rate.setAlignment(QtCore.Qt.AlignRight)
+
+        grid_rates = QtWid.QGridLayout()
+        grid_rates.addWidget(QtWid.QLabel("DAQ:"), 0, 0)
+        grid_rates.addWidget(QtWid.QLabel("chart:"), 1, 0)
+        grid_rates.addWidget(self.qlbl_DAQ_rate, 0, 1)
+        grid_rates.addWidget(self.qlbl_chart_rate, 1, 1)
+        grid_rates.addWidget(QtWid.QLabel("Hz"), 0, 2)
+        grid_rates.addWidget(QtWid.QLabel("Hz"), 1, 2)
+        grid_rates.setAlignment(QtCore.Qt.AlignTop)
+
+        # `Options`
+        self.qpbt_option_1 = QtWid.QPushButton("Option 1")
         self.qpbt_option_1.clicked.connect(self.process_qpbt_option_1)
         self.qpbt_option_2 = QtWid.QPushButton("Option 2")
         self.qpbt_option_2.clicked.connect(self.process_qpbt_option_2)
-        self.qpbt_option_3 = QtWid.QPushButton("Option 3")
-        self.qpbt_option_3.clicked.connect(self.process_qpbt_option_3)
 
         grid = QtWid.QGridLayout()
         grid.addWidget(self.qpbt_option_1, 0, 0)
         grid.addWidget(self.qpbt_option_2, 1, 0)
-        grid.addWidget(self.qpbt_option_3, 2, 0)
         grid.setAlignment(QtCore.Qt.AlignTop)
 
-        qgrp_options = QtWid.QGroupBox("")
+        qgrp_options = QtWid.QGroupBox("Options")
         qgrp_options.setStyleSheet(SS_GROUP)
         qgrp_options.setLayout(grid)
 
         # 'Chart'
+        self.qpbt_pause_chart = QtWid.QPushButton("Pause", checkable=True)
+        self.qpbt_pause_chart.clicked.connect(self.process_qpbt_pause_chart)
         self.qpbt_clear_chart = QtWid.QPushButton("Clear")
         self.qpbt_clear_chart.clicked.connect(self.process_qpbt_clear_chart)
 
         grid = QtWid.QGridLayout()
-        grid.addWidget(self.qpbt_clear_chart, 0, 0)
+        grid.addWidget(self.qpbt_pause_chart, 0, 0)
+        grid.addWidget(self.qpbt_clear_chart, 1, 0)
         grid.setAlignment(QtCore.Qt.AlignTop)
 
         qgrp_chart = QtWid.QGroupBox("Chart")
@@ -93,21 +107,27 @@ class MainWindow(QtWid.QWidget):
         qgrp_chart.setLayout(grid)
 
         vbox = QtWid.QVBoxLayout()
+        vbox.addLayout(grid_rates)
         vbox.addWidget(qgrp_options)
         vbox.addWidget(qgrp_chart)
         vbox.addStretch()
 
-        # Round up bottom frame
-        hbox_bot = QtWid.QHBoxLayout()
-        hbox_bot.addWidget(self.gw_chart, 1)
-        hbox_bot.addLayout(vbox, 0)
+        # Round up frame
+        hbox = QtWid.QHBoxLayout()
+        hbox.addWidget(self.gw_chart, 1)
+        hbox.addLayout(vbox, 0)
 
         # -------------------------
         #   Round up full window
         # -------------------------
 
         vbox = QtWid.QVBoxLayout(self)
-        vbox.addLayout(hbox_bot, stretch=1)
+        vbox.addLayout(hbox, stretch=1)
+
+        # Keep track of the obtained chart refresh rate
+        self.obtained_chart_rate_Hz = np.nan
+        self.qet_chart = QtCore.QElapsedTimer()
+        self.chart_rate_accumulator = 0
 
     # --------------------------------------------------------------------------
     #   Handle controls
@@ -128,18 +148,20 @@ class MainWindow(QtWid.QWidget):
             self.CH_1.clear()
 
     @QtCore.pyqtSlot()
-    def process_qpbt_option_1(self):
+    def process_qpbt_pause_chart(self):
         if timer_chart.isActive():
+            self.qpbt_pause_chart.setText("Unpause")
             timer_chart.stop()
         else:
+            self.qpbt_pause_chart.setText("Pause")
             timer_chart.start()
 
     @QtCore.pyqtSlot()
-    def process_qpbt_option_2(self):
+    def process_qpbt_option_1(self):
         print("option 2")
 
     @QtCore.pyqtSlot()
-    def process_qpbt_option_3(self):
+    def process_qpbt_option_2(self):
         print("option 3")
 
 
@@ -153,24 +175,44 @@ def about_to_quit():
     timer_chart.stop()
 
 
-def DAQ_function():
-    global prev_tick
-    tick = time.perf_counter()
+def update_GUI():
+    window.qlbl_DAQ_rate.setText("%.1f" % qdev.obtained_DAQ_rate_Hz)
 
+
+def update_chart():
+    # Keep track of the obtained chart rate
+    if not window.qet_chart.isValid():
+        window.qet_chart.start()
+    else:
+        window.chart_rate_accumulator += 1
+        dT = window.qet_chart.elapsed()
+
+        if dT >= 1000:  # Evaluate every N elapsed milliseconds
+            window.qet_chart.restart()
+            try:
+                window.obtained_chart_rate_Hz = (
+                    window.chart_rate_accumulator / dT * 1e3
+                )
+            except ZeroDivisionError:
+                obtained_chart_rate_Hz = np.nan
+
+            window.chart_rate_accumulator = 0
+
+    window.qlbl_chart_rate.setText("%.1f" % window.obtained_chart_rate_Hz)
+    window.pi_chart.setTitle("%s points" % f"{(len(window.CH_1._RB_x)):,}")
+
+    window.CH_1.update_curve()
+
+
+def DAQ_function():
     if len(window.CH_1._RB_x) == 0:
         x_0 = 0
     else:
         x_0 = window.CH_1._RB_x[-1]
 
-    # x = np.arange(WORKER_DAQ_INTERVAL_MS * Fs / 1e3) / Fs + time.perf_counter()
     x = (1 + np.arange(WORKER_DAQ_INTERVAL_MS * Fs / 1e3)) / Fs + x_0
-    y = np.sin(2 * np.pi * 100.1 * x)
+    y = np.sin(2 * np.pi * 0.5 * x)
     window.CH_1.add_new_readings(x, y)
-
-    dprint("%.3f DAQ function" % (tick - prev_tick))
-    prev_tick = tick
-
-    # QtWid.QApplication.processEvents()
 
     return True
 
@@ -185,23 +227,24 @@ if __name__ == "__main__":
 
     window = MainWindow()
 
+    # Fake a device that generates data at a sampling rate of `Fs` Hz. The
+    # data gets generated and transferred to our ChartHistory-instance from
+    # out of a separate thread. This is taken care of by QDeviceIO.
     class FakeDevice:
         def __init__(self):
             self.name = "FakeDevice"
             self.is_alive = True
 
-    # Fake a device that generates data at a sampling rate of `Fs` Hz. The
-    # data gets generated and transferred to our ChartHistory-instance from
-    # out of a separate thread. This is taken care of by QDeviceIO.
     qdev = QDeviceIO(dev=FakeDevice())
     qdev.create_worker_DAQ(
         DAQ_interval_ms=WORKER_DAQ_INTERVAL_MS, DAQ_function=DAQ_function
     )
+    qdev.signal_DAQ_updated.connect(update_GUI)
     qdev.start()
 
-    # Chart refresh rate
+    # Chart refresh timer
     timer_chart = QtCore.QTimer(timerType=QtCore.Qt.PreciseTimer)
-    timer_chart.timeout.connect(window.CH_1.update_curve)
+    timer_chart.timeout.connect(update_chart)
     timer_chart.start(CHART_DRAW_INTERVAL_MS)
 
     window.show()
